035ac0f41f21ac4c7096a99bc01bb6c3d6f4e701
Kush Joshi <76556985+KushJoshi16@users.noreply.github.com>
2023-06-06 11:31:12 +0530

    Backend API along with functionality and endpoints completed

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request, Response, abort
+import generator_algo.algorithm as alg
+import json
+import logging
+
+logging.basicConfig(filename="logs/app_logs.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+ 
+logger = logging.getLogger()
+ 
+app = Flask(__name__)
+
+@app.route('/set_inputData',methods=['POST'])
+def set_inputData():
+    input_data  = request.get_json()
+    logger.info("Input data received")
+    f = open("classes/input.json","w")
+    json.dump(input_data, f)
+    f.close()
+    logger.info("Input data stored in input.json")
+    return jsonify(success=True)
+ 
+@app.route('/generate_time_table')
+def generate_time_table():
+    alg.evolutionary_algorithm()
+    logger.info("Time Table Generated")
+    return jsonify(success=True)
+
+@app.route('/get_time_table')
+def get_time_table():
+    try:
+        f = open('classes/output.json',"r")
+        data = json.load(f)
+        f.close()
+    except Exception as e:
+        logger.error(e)
+        try:
+            alg.evolutionary_algorithm()
+            logger.info("Time Table Regenrated")
+            f = open('classes/output.json',"r")
+            data = json.load(f)
+            f.close()
+        except Exception as e:
+            logger.error(e)
+            abort(500)
+    logger.info("Data retrived as data and will be returned as json")
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="generator_algo",
+      version="0.0.1",
+      author="kush",
+      author_email="kushjoshi16@gmail.com",
+      packages=find_packages()
+      )
