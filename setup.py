48f8d1c6db487fcd9f955fcbf97eba14b42e1770
Kush Joshi <76556985+KushJoshi16@users.noreply.github.com>
2023-06-09 19:20:12 +0530

    Input and Output methods modified

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name="generator_algo",
       version="0.0.1",
       author="kush",
       author_email="kushjoshi16@gmail.com",
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=["pandas"]
       )
